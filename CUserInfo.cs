8d0d0f06dde9f75165025cb9d54cdbdf13099e00
noiro500 <yai198x@gmail.com>
2017-07-01 00:22:40 +0300

    Add files via upload

---
+++ CUserInfo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExmoAPI
+{
+    public class CBalances
+    {
+        public string BTC { get; set; }
+        public string USD { get; set; }
+        public string EUR { get; set; }
+        public string RUB { get; set; }
+        public string UAH { get; set; }
+        public string LTC { get; set; }
+        public string DOGE { get; set; }
+        public string DASH { get; set; }
+        public string ETH { get; set; }
+        public string WAVES { get; set; }
+    }
+
+    public class CReserved
+    {
+        public string BTC { get; set; }
+        public string USD { get; set; }
+        public string EUR { get; set; }
+        public string RUB { get; set; }
+        public string UAH { get; set; }
+        public string LTC { get; set; }
+        public string DOGE { get; set; }
+        public string DASH { get; set; }
+        public string ETH { get; set; }
+        public string WAVES { get; set; }
+    }
+
+    public class CUserInfo
+    {
+        public int uid { get; set; }
+        public int server_date { get; set; }
+        public CBalances balances { get; set; }
+        public CReserved reserved { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+using Newtonsoft.Json;
+namespace ExmoAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Init
+            var api = new ExmoApi("K-aef06338eb8ee1794b6cdcde33e53e90d8d4578f", "S-0e84d7277cc01495d3452c5ca23635e24962b2e6");
+
+            //sync query
+            /*var result = api.ApiQuery("user_info", new Dictionary<string, string>());
+            Console.WriteLine("sync result");
+            Console.WriteLine(result);
+            var result2 = api.ApiQuery("user_cancelled_orders", new Dictionary<string, string> { { "limit", "100" }, { "offset", "0"} });
+            Console.WriteLine("sync result2");
+            Console.WriteLine(result2);*/
+
+
+            //async query
+            var task = api.ApiQueryAsync("user_info", new Dictionary<string, string> ());
+            StringBuilder str = new StringBuilder( task.Result.ToString());
+            
+            CUserInfo userInfo = JsonConvert.DeserializeObject<CUserInfo>(str.ToString());
+
+            var task2 = api.ApiQueryAsync("user_open_orders", new Dictionary<string, string>());
+            StringBuilder str2 = new StringBuilder(task2.Result.ToString());
+
+            Console.ReadLine();
+
+            
+
+            Task.Factory.StartNew(async () =>
+            {
+                var result3 = await api.ApiQueryAsync("user_cancelled_orders", new Dictionary<string, string> { { "limit", "2" }, { "offset", "0" } });
+                //Console.WriteLine("async result4");
+                //Console.WriteLine(result3);
+            });
+
+            Console.ReadLine();
+        }
+    }
+}
