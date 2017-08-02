d4192632e3fdaa9c986dbc2df929ccc56f63b2c3
noiro <noiro@NOIRO-PC>
2017-08-02 03:50:35 +0300

    Itades

---
+++ Public_API/CTrades.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ExmoAPI.Public_API
+{
+    public class CTrades : ITrades
+    {
+        [JsonProperty("trade_id")]
+        public int TradeId { get; set; }
+        [JsonProperty("type")]
+        public string Type { get ; set; }
+        [JsonProperty("price")]
+        public decimal Price { get ; set ; }
+        [JsonProperty("quantity")]
+        public decimal Quantity { get ; set ; }
+        [JsonProperty("amount")]
+        public decimal Amount { get ; set ; }
+        [JsonProperty("date")]
+        public int Date { get ; set ; }
+    }
+}
---
+++ Public_API/ITrades.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExmoAPI.Public_API
+{
+    // Список сделок по валютной паре
+    interface ITrades
+    {
+        int TradeId { get; set; }
+        string Type { get; set; }
+        decimal Price { get; set; }
+        decimal Quantity { get; set; }
+        decimal Amount { get; set; }
+        int Date { get; set; }
+    }
+    
+}
