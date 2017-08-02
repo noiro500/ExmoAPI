bfa54712dfe4ac6947754018568f3557935a92d1
noiro <noiro@NOIRO-PC>
2017-08-02 19:43:55 +0300

    Make_CPairSettings

---
+++ Public_API/Classes/CPairSettings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace ExmoAPI.Public_API
+{
+    public class CPairSettings : IPairSettings
+    {
+        [JsonProperty("min_quantity")]
+        public decimal MinQuantity { get; set; }
+        [JsonProperty("max_quantity")]
+        public decimal MaxQuantity { get; set; }
+        [JsonProperty("min_price")]
+        public decimal MinPrice { get; set; }
+        [JsonProperty("max_price")]
+        public decimal MaxPrice { get; set; }
+        [JsonProperty("max_amount")]
+        public decimal MaxAmount { get; set; }
+        [JsonProperty("min_amount")]
+        public decimal MinAmount { get; set; }
+    }
+}
---
+++ Public_API/Interfaces/IPairSettings.cs
@@ -0,0 +1,13 @@
+﻿namespace ExmoAPI.Public_API
+{
+    //Настройки валютных пар
+    public interface IPairSettings
+    {
+        decimal MinQuantity { get; set; }
+        decimal MaxQuantity { get; set; }
+        decimal MinPrice { get; set; }
+        decimal MaxPrice { get; set; }
+        decimal MaxAmount { get; set; }
+        decimal MinAmount { get; set; }
+    }
+}
