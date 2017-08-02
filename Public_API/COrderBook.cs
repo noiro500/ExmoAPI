ae2fca2f07ac372dfef9b4b7ad8d1aecab02570e
noiro <noiro@NOIRO-PC>
2017-08-02 17:47:21 +0300

    good_order_book

---
+++ Public_API/COrderBook.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace ExmoAPI.Public_API
+{
+    public class COrderBook : IOrderBook
+    {
+        [JsonProperty("ask_quantity")]
+        public decimal AskQuantity { get; set ; }
+        [JsonProperty("ask_amount")]
+        public decimal AskAmount { get; set; }
+        [JsonProperty("ask_top")]
+        public decimal AskTop { get; set; }
+        [JsonProperty("bid_quantity")]
+        public decimal BidQuantity { get; set; }
+        [JsonProperty("bid_amount")]
+        public decimal BidAmount { get; set; }
+        [JsonProperty("bid_top")]
+        public decimal BidTop { get; set; }
+        [JsonProperty("ask")]
+        public List<List<decimal>> Ask { get; set; }
+        [JsonProperty("bid")]
+        public List<List<decimal>> Bid { get; set; }
+    }
+}
---
+++ Public_API/IOrderBook.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace ExmoAPI.Public_API
+{
+    //Книга ордеров по валютной паре
+    public interface IOrderBook
+    {
+        decimal AskQuantity { get; set; }
+        decimal AskAmount { get; set; }
+        decimal AskTop { get; set; }
+        decimal BidQuantity { get; set; }
+        decimal BidAmount { get; set; }
+        decimal BidTop { get; set; }
+        List<List<decimal>> Ask { get; set; }
+        List<List<decimal>> Bid { get; set; }
+    }
+}
