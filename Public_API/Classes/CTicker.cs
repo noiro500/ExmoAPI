b7335bcc9c6cc9f5e93b19eaba0dfd53af7beef0
noiro <noiro@NOIRO-PC>
2017-08-02 19:32:50 +0300

    Make_Ticker

---
+++ Public_API/Classes/CTicker.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace ExmoAPI.Public_API
+{
+    public class CTicker : ITicker
+    {
+        [JsonProperty("buy_price")]
+        public decimal BuyPrice { get  ; set  ; }
+        [JsonProperty("sell_price")]
+        public decimal SellPrice { get  ; set  ; }
+        [JsonProperty("last_trade")]
+        public decimal LastTrade { get  ; set  ; }
+        [JsonProperty("high")]
+        public decimal High { get  ; set  ; }
+        [JsonProperty("low")]
+        public decimal Low { get  ; set  ; }
+        [JsonProperty("avg")]
+        public decimal Avg { get  ; set  ; }
+        [JsonProperty("vol")]
+        public decimal Vol { get  ; set  ; }
+        [JsonProperty("vol_curr")]
+        public decimal VolCurr { get  ; set  ; }
+        [JsonProperty("updated")]
+        public int Updated { get  ; set  ; }
+    }
+}
---
+++ Public_API/Interfaces/ITicker.cs
@@ -0,0 +1,16 @@
+﻿namespace ExmoAPI.Public_API
+{
+    //Cтатистика цен и объемов торгов по валютным парам
+    public interface ITicker
+    {
+        decimal BuyPrice { get; set; }
+        decimal SellPrice { get; set; }
+        decimal LastTrade { get; set; }
+        decimal High { get; set; }
+        decimal Low { get; set; }
+        decimal Avg { get; set; }
+        decimal Vol { get; set; }
+        decimal VolCurr { get; set; }
+       int Updated { get; set; }
+    }
+}
