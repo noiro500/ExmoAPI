5fd7009aeab40648d643e054b8f73c47d34528f4
noiro <noiro@NOIRO-PC>
2017-08-03 04:19:37 +0300

    Make_User_Info

---
+++ Authenticated_API/Classes/CUserInfo.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using ExmoAPI.Authenticated_API.Interfeces;
+using Newtonsoft.Json;
+
+namespace ExmoAPI.Authenticated_API.Classes
+{
+    //Получение информации об аккаунте пользователя
+    public class CUserInfo
+    {
+        [JsonProperty("uid")]
+        public int Uid { get; set; }
+        [JsonProperty("server_date")]
+        public int ServerDate { get; set; }
+        [JsonProperty("balances")]
+        public Balances Balances { get; set; }
+        [JsonProperty("reserved")]
+        public Reserved Reserved { get; set; }
+    }
+
+    public class Balances : IBalances
+    {
+        [JsonProperty("USD")]
+        public decimal Usd { get; set; }
+        [JsonProperty("EUR")]
+        public decimal Eur { get; set; }
+        [JsonProperty("RUB")]
+        public decimal Rub { get; set; }
+        [JsonProperty("UAH")]
+        public decimal Uah { get; set; }
+        [JsonProperty("BTC")]
+        public decimal Btc { get; set; }
+        [JsonProperty("LTC")]
+        public decimal Ltc { get; set; }
+        [JsonProperty("DOGE")]
+        public decimal Doge { get; set; }
+        [JsonProperty("DASH")]
+        public decimal Dash { get; set; }
+        [JsonProperty("ETH")]
+        public decimal Eth { get; set; }
+        [JsonProperty("WAVES")]
+        public decimal Waves { get; set; }
+        [JsonProperty("ZEC")]
+        public decimal Zec { get; set; }
+    }
+    public class Reserved : IReserved
+    {
+        [JsonProperty("USD")]
+        public decimal Usd { get; set; }
+        [JsonProperty("EUR")]
+        public decimal Eur { get; set; }
+        [JsonProperty("RUB")]
+        public decimal Rub { get; set; }
+        [JsonProperty("UAH")]
+        public decimal Uah { get; set; }
+        [JsonProperty("BTC")]
+        public decimal Btc { get; set; }
+        [JsonProperty("LTC")]
+        public decimal Ltc { get; set; }
+        [JsonProperty("DOGE")]
+        public decimal Doge { get; set; }
+        [JsonProperty("DASH")]
+        public decimal Dash { get; set; }
+        [JsonProperty("ETH")]
+        public decimal Eth { get; set; }
+        [JsonProperty("WAVES")]
+        public decimal Waves { get; set; }
+        [JsonProperty("ZEC")]
+        public decimal Zec { get; set; }
+    }
+}
---
+++ Authenticated_API/Interfeces/IUserInfo.cs
@@ -0,0 +1,39 @@
+﻿namespace ExmoAPI.Authenticated_API.Interfeces
+{
+    public interface IBalances
+    {
+        decimal Usd { get; set; }
+        decimal Eur { get; set; }
+        decimal Rub { get; set; }
+        decimal Uah { get; set; }
+        decimal Btc { get; set; }
+        decimal Ltc { get; set; }
+        decimal Doge { get; set; }
+        decimal Dash { get; set; }
+        decimal Eth { get; set; }
+        decimal Waves { get; set; }
+        decimal Zec { get; set; }
+    }
+    public interface IReserved
+    {
+        decimal Usd { get; set; }
+        decimal Eur { get; set; }
+        decimal Rub { get; set; }
+        decimal Uah { get; set; }
+        decimal Btc { get; set; }
+        decimal Ltc { get; set; }
+        decimal Doge { get; set; }
+        decimal Dash { get; set; }
+        decimal Eth { get; set; }
+        decimal Waves { get; set; }
+        decimal Zec { get; set; }
+    }
+
+    public interface IUserInfo
+    {
+       int Uid { get; set; }
+        int ServerDate { get; set; }
+        IBalances Balances { get; set; }
+        IReserved Reserved { get; set; }
+    }
+}
