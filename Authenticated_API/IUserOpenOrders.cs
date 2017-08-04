142d65a1ffdbeb8db1b72fee2fb4c23be0b47f07
noiro <noiro@NOIRO-PC>
2017-08-05 00:39:54 +0300

    make_user_open_orders

---
+++ Authenticated_API/IUserOpenOrders.cs
@@ -0,0 +1,14 @@
+﻿namespace ExmoAPI.Authenticated_API
+{
+    //Получение списока открытых ордеров пользователя
+    public interface IUserOpenOrders
+    {
+        decimal OrderId { get;}
+        decimal Created { get;}
+        string Type { get;}
+        string Pair { get;}
+        decimal Price { get;}
+        decimal Quantity { get;}
+        decimal Amount { get;}
+    }
+}
