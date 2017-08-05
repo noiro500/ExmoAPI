34e6c12bf57722678ff770333cf1fdb17e67a702
noiro <noiro@NOIRO-PC>
2017-08-05 03:06:19 +0300

    make_order_trades

---
+++ Authenticated_API/Interfeces/IOrderTrades.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace ExmoAPI.Authenticated_API.Interfeces
+{
+    //Получение истории сделок ордера
+    public interface IOrderTrades
+    {
+        string Type { get;}
+        string InCurrency { get;}
+        decimal InAmount { get;}
+        string OutCurrency { get;}
+        decimal OutAmount { get;}
+        //List<ITrade> Trades { get;}
+    }
+
+    public interface ITrade
+    {
+        decimal TradeId { get;}
+        decimal Date { get;}
+        string Type { get;}
+        string Pair { get;}
+        decimal OrderId { get;}
+        decimal Quantity { get;}
+        decimal Price { get;}
+        decimal Amount { get;}
+    }
+}
