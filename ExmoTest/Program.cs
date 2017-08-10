9d2d5cb291c54f2373c35dc8bc8ca7a322593d61
Lenovo <Lenovo@LENOVO-PC>
2017-08-10 03:16:03 +0300

    Add ExmoTest

---
+++ ExmoTest/Program.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using ExmoAPI;
+using ExmoAPI.Authenticated_API.Classes;
+using ExmoAPI.Generic;
+using ExmoAPI.Public_API.Classes;
+using ExmoAPI.Public_API.Interfaces;
+
+namespace ExmoTest
+{
+class Program
+{
+static void Main(string[] args)
+{
+var api = new ExmoApi("K-152c412836b5cc48671d20f2a48e59990057a22c", "S-6c0ac88984cab15fdbd5af301e1348d7b925a34d");
+string currentPair = "LTC_RUB";
+
+
+#region Public_API
+
+#region trades
+
+var jsonTrades = api.ApiQueryAsync("trades", new Dictionary<string, string>(), currentPair);
+var objTrades = JObject.Parse(jsonTrades.Result.ToString());
+var objArray = JsonConvert.DeserializeObject<CTrades[]>(objTrades[currentPair].ToString());
+
+
+#endregion
+
+#region order_book
+
+var jsonOrderBook = api.ApiQueryAsync("order_book", new Dictionary<string, string>(), currentPair);
+var objOrderBook = JObject.Parse(jsonOrderBook.Result.ToString());
+var objArrayOrderBook = JsonConvert.DeserializeObject<COrderBook>(objOrderBook[currentPair].ToString());
+
+
+#endregion
+
+#region ticker
+
+var jsonTicker = api.ApiQueryAsync("ticker", new Dictionary<string, string>());
+var objTicker = JObject.Parse(jsonTicker.Result.ToString());
+var objArrayTicker = JsonConvert.DeserializeObject<CTicker>(objTicker[currentPair].ToString());
+
+#endregion
+
+#region pair_settings
+
+var jsonPairSettings = api.ApiQueryAsync("pair_settings", new Dictionary<string, string>());
+var objPairSettings = JObject.Parse(jsonPairSettings.Result.ToString());
+var objArrayPairSettings = JsonConvert.DeserializeObject<CPairSettings>(objPairSettings[currentPair].ToString());
+
+#endregion
+
+#endregion
+
+#region Authenticated_API
+
+#region user_info
+
+var jsonUserInfo = api.ApiQueryAsync("user_info", new Dictionary<string, string>());
+var objUserInfo = JObject.Parse(jsonUserInfo.Result.ToString());
+var objArrayUserInfo = JsonConvert.DeserializeObject<CUserInfo>(objUserInfo.ToString());
+
+#endregion
+
+#region order_create
+/*
+var pair = currentPair;
+var quantity = 0.17;
+var price = 2800;
+var type = "sell";
+var jsonOrderCreate = api.ApiQueryAsync("order_create",
+    new Dictionary<string, string>()
+    {
+        {"pair", pair},
+        {"quantity", quantity.ToString(CultureInfo.InvariantCulture)},
+        {"price", price.ToString(CultureInfo.InvariantCulture)},
+        {"type", type}
+    });
+
+var objOrderCreate = JObject.Parse(jsonOrderCreate.Result.ToString());
+var objArrayOrderCreate = JsonConvert.DeserializeObject<COrderCreate>(objOrderCreate.ToString());
+*/
+#endregion
+
+#region order_cancel
+/*
+var jsonOrderCancel = api.ApiQueryAsync("order_cancel",
+    new Dictionary<string, string>()
+    {
+        {"order_id", objArrayOrderCreate.OrderId.ToString(CultureInfo.InvariantCulture)}
+    });
+var objOrderCansel = JObject.Parse(jsonOrderCancel.Result.ToString());
+var objArrayOrderCansel = JsonConvert.DeserializeObject<COrderCancel>(objOrderCansel.ToString());
+*/
+#endregion
+
+#region user_open_orders
+
+var jsonUserOpenOrders = api.ApiQueryAsync("user_open_orders", new Dictionary<string, string>());
+var objUserOpenOrders = JObject.Parse(jsonUserOpenOrders.Result.ToString());
+CUserOpenOrders[] objArrayUserOpenOrders;
+
+foreach (var c in CCurrency.GetCurrencyPairList(api))
+{
+if (objUserOpenOrders.ToString().Contains(c))
+{
+objArrayUserOpenOrders =
+JsonConvert.DeserializeObject<CUserOpenOrders[]>(objUserOpenOrders[c].ToString());
+foreach (var f in objArrayUserOpenOrders)
+{
+Console.WriteLine(" " + c);
+Console.WriteLine(f.OrderId.ToString(CultureInfo.InvariantCulture));
+Console.WriteLine((new DateTime(1970, 1, 1, 0, 0, 0, 0)).AddSeconds(Convert.ToDouble(f.Created)));
+Console.WriteLine(f.Type.ToString(CultureInfo.InvariantCulture));
+Console.WriteLine(f.Pair.ToString(CultureInfo.InvariantCulture));
+Console.WriteLine(f.Price.ToString(CultureInfo.InvariantCulture));
+Console.WriteLine(f.Quantity.ToString(CultureInfo.InvariantCulture));
+Console.WriteLine(f.Amount.ToString(CultureInfo.InvariantCulture));
+}
+}
+}
+
+#endregion
+
+#region user_trades
+
+pair = "BTC_RUB";
+var limit = 10;
+var offcet = 0;
+var jsonUserTrades = api.ApiQueryAsync("user_trades",
+new Dictionary<string, string>()
+{
+{"pair", pair},
+{"limit", limit.ToString(CultureInfo.InvariantCulture)},
+{"offcet", offcet.ToString(CultureInfo.InvariantCulture)}
+});
+var objUserTrades = JObject.Parse(jsonUserTrades.Result.ToString());
+var objArrayUserTrades = JsonConvert.DeserializeObject<CUserTrades[]>(objUserTrades[pair].ToString());
+//Сделать как с user_open_orders
+#endregion
+#region user_cancelled_orders
+
+var jsonUserCancelledOrders = api.ApiQueryAsync("user_cancelled_orders",
+new Dictionary<string, string>()
+{
+{"limit", 10.ToString(CultureInfo.InvariantCulture)},
+{"offcet", 0.ToString(CultureInfo.InvariantCulture)}
+});
+var objUserCancelledOrders = JArray.Parse(jsonUserCancelledOrders.Result.ToString());
+var objArrayUserCancelledOrders = JsonConvert.DeserializeObject<CUserCancelledOrders[]>(objUserCancelledOrders.ToString());
+
+#endregion
+
+#region order_trades
+
+var orderId = objArrayUserTrades[0].OrderId;
+var jsonOrderTrades = api.ApiQueryAsync("order_trades",
+new Dictionary<string, string>()
+{
+{"order_id", orderId.ToString(CultureInfo.InvariantCulture)}
+});
+var objOrderTrades = JObject.Parse(jsonOrderTrades.Result.ToString());
+var objArrayOrderTrades = JsonConvert.DeserializeObject<COrderTrades>(objOrderTrades.ToString());
+
+#endregion
+
+#region required_amount
+
+pair = "USD_RUB";
+var quant = 7;
+var jsonRequiredAmount = api.ApiQueryAsync("required_amount",
+new Dictionary<string, string>()
+{
+{"pair", pair},
+{"quantity", quant.ToString(CultureInfo.InvariantCulture) }
+});
+var objRequiredAmount = JObject.Parse(jsonRequiredAmount.Result.ToString());
+var objArrayRequiredAmount = JsonConvert.DeserializeObject<CRequiredAmount>(objRequiredAmount.ToString());
+
+#endregion
+
+#endregion
+
+IHelperPublicAPI<CPairSettings> test = new CHelperPublicAPI<CPairSettings>();
+test.GetResult("pair_settings", currentPair, api);
+var a = test.ResultList;
+}
+}
+}
