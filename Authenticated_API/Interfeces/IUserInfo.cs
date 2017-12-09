23d0ffb9b629e59112c1ce1f2df095be928c6f30
noiro <noiro@NOIRO-PC>
2017-12-09 04:07:49 +0300

    add commit

--- Authenticated_API/Interfeces/IUserInfo.cs
+++ Authenticated_API/Interfeces/IUserInfo.cs
@@ -6,6 +6,7 @@
         decimal Usd { get;}
         decimal Eur { get;}
         decimal Rub { get;}
+        decimal Pln { get; }
         decimal Uah { get;}
         decimal Btc { get;}
         decimal Ltc { get;}
@@ -13,13 +14,22 @@
         decimal Dash { get;}
         decimal Eth { get;}
         decimal Waves { get;}
-        decimal Zec { get;}
+        decimal Zec { get; }
+        decimal Usdt { get;}
+        decimal Xmr { get; }
+        decimal Xrp { get; }
+        decimal Kick { get; }
+        decimal Etc { get; }
+        decimal Bch { get; }
+        
+
     }
     public interface IReserved
     {
         decimal Usd { get;}
         decimal Eur { get;}
         decimal Rub { get;}
+        decimal Pln { get; }
         decimal Uah { get;}
         decimal Btc { get;}
         decimal Ltc { get;}
@@ -28,12 +38,18 @@
         decimal Eth { get;}
         decimal Waves { get;}
         decimal Zec { get;}
+        decimal Usdt { get; }
+        decimal Xmr { get; }
+        decimal Xrp { get; }
+        decimal Kick { get; }
+        decimal Etc { get; }
+        decimal Bch { get; }
     }
 
     public interface IUserInfo
     {
        int Uid { get;}
-        int ServerDate { get;}
+        ulong ServerDate { get;}
         IBalances Balances { get;}
         IReserved Reserved { get;}
     }
